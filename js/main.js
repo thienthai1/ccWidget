ebbe722d3bd3ee614cd529956225862f98b2740c
thienthai1 <surkessone@gmail.com>
2021-06-22 23:03:30 +0700

    push lastest

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 $(document).ready(function(){
 
-    $('.btnSubmit').click(function() {
-        var baseCurrency = $('#baseCurrency').val();
-        var finalCurrency = $('#finalCurrency').val();
-        var baseCurrencyPrice = $('#baseCurrencyPrice').val();
-        var finalCurrencyPrice = $('#finalCurrencyPrice').val();
-        var myPrice = $('#myPrice').val();
+    $.get( "https://raw.githubusercontent.com/thienthai1/ccWidget/master/js/main.txt", function( data ) {
+        
+        $("#ccWidgetContent").append(data);
 
-        var cal = ((baseCurrencyPrice * myPrice)/finalCurrencyPrice).toFixed(5);
+        $('.btnSubmit').click(function() {
+            var baseCurrency = $('#baseCurrency').val();
+            var finalCurrency = $('#finalCurrency').val();
+            var baseCurrencyPrice = $('#baseCurrencyPrice').val();
+            var finalCurrencyPrice = $('#finalCurrencyPrice').val();
+            var myPrice = $('#myPrice').val();
+    
+            var cal = ((baseCurrencyPrice * myPrice)/finalCurrencyPrice).toFixed(5);
+    
+            $( "#result" ).empty();
+            $( "#result" ).append( cal + " " );
+            $( "#baseCurrencyVal2" ).empty();
+            $( "#baseCurrencyVal2" ).append( finalCurrency );
+    
+        });
+    
+        $( "#baseCurrency" ).change(function() {
+            var baseCurrency = $('#baseCurrency').val();
+            $( "#baseCurrencyVal" ).empty();
+            $( "#baseCurrencyVal" ).append( baseCurrency );
+        });
 
-        $( "#result" ).empty();
-        $( "#result" ).append( cal + " " );
-        $( "#baseCurrencyVal2" ).empty();
-        $( "#baseCurrencyVal2" ).append( finalCurrency );
-
-    });
-
-    $( "#baseCurrency" ).change(function() {
-        var baseCurrency = $('#baseCurrency').val();
-        $( "#baseCurrencyVal" ).empty();
-        $( "#baseCurrencyVal" ).append( baseCurrency );
-    });
-
-    $.get( "js/main.txt", function( data ) {
-        alert( data );
     });
 
 });
