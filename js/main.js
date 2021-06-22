de26be7a0cf24c25f8dd6f8e414b5cc94ca55bd6
thienthai1 <surkessone@gmail.com>
2021-06-22 20:49:33 +0700

    push lastets

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-    $("p").click(function(){
-      $(this).hide();
+
+    $('.btnSubmit').click(function() {
+        var baseCurrency = $('#baseCurrency').val();
+        var finalCurrency = $('#finalCurrency').val();
+        var baseCurrencyPrice = $('#baseCurrencyPrice').val();
+        var finalCurrencyPrice = $('#finalCurrencyPrice').val();
+        var myPrice = $('#myPrice').val();
+
+        var cal = (baseCurrencyPrice * myPrice)/finalCurrencyPrice;
+
+        $( "#result" ).empty();
+        $( "#result" ).append( cal + " " );
+        $( "#baseCurrencyVal2" ).empty();
+        $( "#baseCurrencyVal2" ).append( baseCurrency );
+
     });
-  });
+
+    $( "#baseCurrency" ).change(function() {
+        var baseCurrency = $('#baseCurrency').val();
+        $( "#baseCurrencyVal" ).empty();
+        $( "#baseCurrencyVal" ).append( baseCurrency );
+    });
+
+});
